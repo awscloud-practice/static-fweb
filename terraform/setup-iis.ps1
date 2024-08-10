b5a23bfe33c6bf984d7500213eab6994cbe92598
awscloud-practice <awscloud2002@gmail.com>
2024-08-10 21:09:26 +0530

    Create setup-iis.ps1

---
+++ terraform/setup-iis.ps1
@@ -0,0 +1,32 @@
+# Ensure IIS is installed
+Install-WindowsFeature -Name Web-Server -IncludeManagementTools;
+
+# Ensure the folder exists
+$siteName = 'DuplicateSite'
+$sitePath = 'C:\inetpub\wwwroot\DuplicateSite'
+$bindingIP = '*'
+$bindingPort = 8090
+
+Write-Output "Ensuring folder exists at: $sitePath"
+if (-not (Test-Path $sitePath)) {
+    New-Item -Path $sitePath -ItemType Directory
+    Write-Output "Created directory: $sitePath"
+} else {
+    Write-Output "Directory already exists: $sitePath"
+}
+
+Write-Output "Checking if site exists: $siteName"
+$site = Get-Website | Where-Object { $_.Name -eq $siteName }
+
+if ($null -eq $site) {
+    Write-Output "Site does not exist. Creating site: $siteName"
+    New-Website -Name $siteName -PhysicalPath $sitePath -Port $bindingPort
+} else {
+    Write-Output "Site exists. Updating site: $siteName"
+    Set-ItemProperty "IIS:\Sites\$siteName" -Name physicalPath -Value $sitePath
+    Remove-WebBinding -Name $siteName -IPAddress $bindingIP -Port $bindingPort
+    New-WebBinding -Name $siteName -IPAddress $bindingIP -Port $bindingPort
+    Start-Website -Name $siteName
+}
+
+Write-Output "Completed IIS setup for: $siteName"
